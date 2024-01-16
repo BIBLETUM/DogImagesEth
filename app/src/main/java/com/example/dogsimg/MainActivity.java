faee9fe0584cb0638e7f8768c7e0c867fccc7cec
BIBLETUM <pasha.remizov@mail.ru>
2024-01-16 15:13:38 +0300

    Error handling implemented

--- app/src/main/java/com/example/dogsimg/MainActivity.java
+++ app/src/main/java/com/example/dogsimg/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dogsimg;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
@@ -7,9 +9,11 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -39,18 +43,45 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.loadDogImage();
+        viewModel.getIsSuccess().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean isSuccess) {
+                if (isSuccess) {
+
+                } else {
+                    Toast.makeText(MainActivity.this, "404", LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewModel.getIsLoading().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean loading) {
+                if (loading) {
+                    dogProgressBar.setVisibility(View.VISIBLE);
+                } else {
+                    dogProgressBar.setVisibility(View.GONE);
+                }
+            }
+        });
         viewModel.getDogImage().observe(this, new Observer<DogImage>() {
             @Override
             public void onChanged(DogImage dogImage) {
                 Glide.with(MainActivity.this)
                         .load(dogImage.getMessage())
                         .into(dogImageView);
-            Log.d(TAG, dogImage.getMessage());
+                Log.d(TAG, dogImage.getMessage());
+            }
+        });
+
+        dogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.loadDogImage();
             }
         });
     }
 
-    private void initViews(){
+    private void initViews() {
         dogImageView = findViewById(R.id.dogImageView);
         dogButton = findViewById(R.id.dogButton);
         dogProgressBar = findViewById(R.id.dogProgressBar);
--- app/src/main/java/com/example/dogsimg/MainViewModel.java
+++ app/src/main/java/com/example/dogsimg/MainViewModel.java
@@ -23,6 +23,7 @@ import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.disposables.DisposableContainer;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -33,9 +34,19 @@ public class MainViewModel extends AndroidViewModel {
     private static final String KEY_STATUS = "status";
     private static final String TAG = "MainViewModel";
     private MutableLiveData<DogImage> dogImage = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isSuccess = new MutableLiveData<>();
 
     public LiveData<DogImage> getDogImage() {
         return dogImage;
+    }
+
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
+    }
+
+    public LiveData<Boolean> getIsSuccess() {
+        return isSuccess;
     }
 
     public MainViewModel(@NonNull Application application) {
@@ -43,9 +54,23 @@ public class MainViewModel extends AndroidViewModel {
     }
 
     public void loadDogImage() {
+
         Disposable disposable = loadImageRx()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Consumer<Disposable>() {
+                    @Override
+                    public void accept(Disposable disposable) throws Throwable {
+                        isLoading.setValue(true);
+                        isSuccess.setValue(true);
+                    }
+                })
+                .doAfterTerminate(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        isLoading.setValue(false);
+                    }
+                })
                 .subscribe(new Consumer<DogImage>() {
                     @Override
                     public void accept(DogImage image) throws Throwable {
@@ -55,6 +80,7 @@ public class MainViewModel extends AndroidViewModel {
                     @Override
                     public void accept(Throwable throwable) throws Throwable {
                         Log.d(TAG, throwable.getMessage());
+                        isSuccess.setValue(false);
                     }
                 });
         disposables.add(disposable);
